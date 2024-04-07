a37621b0545e09b2d8e9b5245b5f1df6caa9cead
watchirapong <125724891+watchirapong@users.noreply.github.com>
2024-04-08 06:20:19 +0700

    camerafollow

---
+++ GAME2DEIEI/Assets/CamMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Vector3 offset;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = player.position + offset;
+        transform.position= Vector3.Lerp(transform.position,desiredPos,speed * Time.deltaTime);
+    }
+}
