744b31690ab4ef97e4b813f1a1b1f8c165c6e2c2
watchirapong <125724891+watchirapong@users.noreply.github.com>
2024-04-08 07:05:25 +0700

    commit pai gone

--- GAME2DEIEI/Assets/PlayerMovement.cs
+++ GAME2DEIEI/Assets/PlayerMovement.cs
@@ -27,18 +27,18 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D other)
+private void OnCollisionEnter2D(Collision2D other)
+{
+    if (other.gameObject.CompareTag("Floor"))
     {
-        if (other.gameObject.CompareTag("Floor"))
-        {
-            isJumping = false;
-        }
+        isJumping = false;
     }
-    private void OnCollisionExit2D(Collision2D other)
+}
+private void OnCollisionExit2D(Collision2D other)
+{
+    if (other.gameObject.CompareTag("Floor"))
     {
-        if (other.gameObject.CompareTag("Floor"))
-        {
-            isJumping = true;
-        }
+        isJumping = true;
     }
+}
 }
