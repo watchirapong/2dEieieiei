7b37076c52198543ecd5e33edd2a088be678eb47
watchirapong <125724891+watchirapong@users.noreply.github.com>
2024-04-11 05:59:45 +0700

    OK

--- GAME2DEIEI/Assets/PlayerMovement.cs
+++ GAME2DEIEI/Assets/PlayerMovement.cs
@@ -2,43 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float speed;
-    public float jump;
-    private float Move;
-    public Rigidbody2D rb;
-    // Start is called before the first frame update
-    public bool isJumping;
-    void Start()
-    {
+public class PlayerMovement : MonoBehaviour {
 
-    }
+	public CharacterController2D controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-        if (Input.GetButtonDown("Jump") && isJumping == false)
-        {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-        }
-    }
+	public float runSpeed = 40f;
 
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	
+	// Update is called once per frame
+	void Update () {
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Floor"))
-        {
-            isJumping = false;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Floor"))
-        {
-            isJumping = true;
-        }
-    }
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
